92dbf6d06b32fe9740c5fa6e18b6cf2383306c84
logschan <logschan@gmail.com>
2021-03-23 07:50:29 -0600

    updating js file

--- src/script.js
+++ src/script.js
@@ -1,83 +1,60 @@
-function cityDate(date) {
-  let now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+//formatting the date and time
+function returnDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[date.getDay()];
+
+  return `${day}, ${formatDate(timestamp)}`;
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+    if  (hours < 10) {
+      hours = `0${hours}`;
+    }
+  let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`
+    }
+
+    return `${hours}:${minutes}`;
+}
+  
+  //updating temperature and city
+  function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let dateElement = document.querySelector("#date");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#windSpeed");
+
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    dateElement.innerHTML = returnDate(reponse.data.dt);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windSpeedElement.innerHTML = Math.round(reponse.data.wind.speed);
+  }
+  
+  //calling the api
+  function search(city) {
+  let apiKey = "5c043c30014fadd9b81c029b76e781d4";
+  let cityName = "New York";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
   }
 
-  let weeks = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
+  function enterCity(event) {
+    event.preventDefault();
+    let enterCityInput = document.querySelector("#search-input");
+    search(enterCityInput.value);
+  }
 
-  let day = weeks[date.getDay()];
-
-  return `${day} ${hours}:${minutes}`;
-}
-
-//creating an event to search for a city
-function searchInput(event) {
-  event.preventDefault();
-  let currentCity = document.querySelector("#current-city");
-  let inputCity = document.querySelector("#search-input");
-  currentCity.innerHTML = inputCity.value;
-}
-
-//updating searched city's date and time
-let currentDate = document.querySelector("#current-date");
-let currentTime = new Date();
-currentDate.innerHTML = cityDate(currentTime);
-
-//updating city using searchInput function
-let enteredCity = document.querySelector("#search-form");
-enteredCity.addEventListener("submit", searchInput);
-
-function showWeather(response) {
-  document.querySelector("#current-city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-}
-
-function findCity(city) {
-  let key = "a7ef2b762f0c823efb0f59438b2d5e2e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
-}
-
-function submit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-input").value;
-  findCity(city);
-}
-
-function getLocation(position) {
-  let key = "a7ef2b762f0c823efb0f59438b2d5e2e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(getLocation);
-}
-
-let dateElement = document.querySelector("#current-date");
-let timeElement = new Date();
-dateElement.innerHTML = cityDate(timeElement);
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", submit);
-
-let currentLocationButton = document.querySelector("#current-btn");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-findCity("New York");
+  let formSubmit = document.querySelector("#search-form");
+  formSubmit.addEventListener("submit", enterCity);
